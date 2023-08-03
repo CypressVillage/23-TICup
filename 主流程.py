fe745ec9b3c284f244b0401e22dc61f9b5e793a5
cypress <zhaobc3@mail2.sysu.edu.cn>
2023-08-03 11:32:37 +0800

    ⚡️ 识别红点分为两种阈值

--- 主流程.py
+++ 主流程.py
@@ -5,11 +5,13 @@ from pyb import Pin
 '''常量定义'''
 white_background_size_min = 3000 # 白纸背景最小面积
 '''阈值定义'''
-thresholds_redpoint = [
+thresholds_redpoint_base = [
 (48, 75, 36, 74, -15, 25), # 白板红光，方框比较小，但是不能跟踪黑色部分👍
-(0, 100, 15, 53, -30, 40), # 3号上午黑线红点👍
 #(95, 100, -5, 5, -5, 5), # 全白
 #(0, 100, 21, 127, -38, 127), # 白板黑胶带红光，都可以跟踪，但是方框比较大
+]
+thresholds_redpoint_blackline = [
+(0, 100, 15, 53, -30, 40), # 3号上午黑线红点👍
 ]
 thresholds_whitebackground = [
 (40, 78, -19, 4, -22, -3), # 3号上午白纸背景
@@ -30,25 +32,44 @@ clock = time.clock()
 
 
 def find_red_point():
-    '''找到红点的坐标，返回x, y'''
+    '''
+    找到红点的坐标，返回x, y
+    
+    如果使用第一种阈值就能找到红点，就使用第一种阈值，否则使用第二种阈值的均值
+    '''
     x, y, sumx, sumy = 0, 0, 0, 0
     find_point = False
     blobs = []
+    blob_0, blob_1, blob_2, blob_3 = 0, 0, 0, 0
     while not find_point:
         img = sensor.snapshot()
         clock.tick() # 用于计算FPS
-        blobs = img.find_blobs(thresholds_redpoint)
+        # 用第一种阈值找红点
+        blobs = img.find_blobs(thresholds_redpoint_base)
+        if blobs and blobs[0]:
+            x, y = blobs[0].cx(), blobs[0].cy()
+            blob_0, blob_1, blob_2, blob_3 = blobs[0][0], blobs[0][1], blobs[0][2], blobs[0][3]
+            break
+
+        # 用第二种阈值找红点
+        blobs = img.find_blobs(thresholds_redpoint_blackline)
         if blobs:
             for blob in blobs:
-                img.draw_rectangle(blob[0:4]) # rect
-                img.draw_cross(blob[5], blob[6]) # cx, cy
                 sumx += blob[5]
                 sumy += blob[6]
+                blob_0 += blob[0]
+                blob_1 += blob[1]
+                blob_2 += blob[2]
+                blob_3 += blob[3]
             x, y = int(sumx / len(blobs)), int(sumy / len(blobs))
+            blob_0, blob_1, blob_2, blob_3 = int(blob_0 / len(blobs)), int(blob_1 / len(blobs)), int(blob_2 / len(blobs)), int(blob_3 / len(blobs))
             print('find red point: ', x, y)
             find_point = True
         else:
             print("find red point: not found")
+    # 画出红点的外接矩形
+    img.draw_rectangle(blob_0, blob_1, blob_2, blob_3)
+    img.draw_cross(x, y)
     return x, y
 
 def find_white_background():
